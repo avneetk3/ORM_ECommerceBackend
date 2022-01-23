0561b174a381150a94773ce57fed395a0d14e26b
avneetk3 <92407474+avneetk3@users.noreply.github.com>
2022-01-23 12:43:19 -0500

    removed body parser
    removed body parser

--- Develop/server.js
+++ Develop/server.js
@@ -6,23 +6,11 @@ const sequelize = require('./config/connection')
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const bodyParser = require('body-parser');
-//app.use(require('connect').bodyParser);
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-
-
-
-/*app.use(express.json());
-app.use(express.urlencoded({ extended: true }));*/
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
-
-// sync sequelize models to the database, then turn on the server
-/*app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});*/
 
 
 sequelize.sync({ force: false }).then(() => {  //force true clear out DB, delete tables , false will not delete everything
